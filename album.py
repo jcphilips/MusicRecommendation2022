eab5e4c10024c1442d1d709575273b2fc874ee4f
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2023-07-02 19:05:46 +0530

    Fix: allow assigning of multiple genres to an album

--- album.py
+++ album.py
@@ -1,8 +1,8 @@
 class Album:
-    def __init__(self, album_title, artist, genre, review, rating, label) -> None:
+    def __init__(self, album_title, artist, genres, review, rating, label) -> None:
         self.album_title = album_title
         self.artist = artist
-        self.genre = genre
+        self.genres = genres
         self.rating = rating
         self.label = label
         self.review = review
