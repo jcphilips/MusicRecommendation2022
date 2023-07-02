d1b684bc57974e0db5b44dcddea3135be00c50a1
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2023-07-02 18:25:24 +0530

    typo: fix album class name

--- album.py
+++ album.py
@@ -1,5 +1,5 @@
-class Albums:
-    def __init__(self, album_title, artist, genre, review, rating) -> None:
+class Album:
+    def __init__(self, album_title, artist, genre, review, rating, label) -> None:
         self.album_title = album_title
         self.artist = artist
         self.genre = genre
