f515268fd65215dbf0fbbd13c959361e9f419693
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2023-07-03 11:59:17 +0530

    Adjust Album class
    Adjust class to better relect ordering of json data

--- album.py
+++ album.py
@@ -1,9 +1,8 @@
 class Album:
-    def __init__(self, album_title, artist, genres, review, rating, label) -> None:
+    def __init__(self, album_title, artist, genres, label, rating, review) -> None:
         self.album_title = album_title
         self.artist = artist
         self.genres = genres
-        self.rating = rating
         self.label = label
+        self.rating = rating
         self.review = review
-        
