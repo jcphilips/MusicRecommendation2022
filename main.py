84d22db27a9f8e8c8871bfb950313399877088b8
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2023-07-03 12:19:51 +0530

    Feat: Create function to generate album graph

--- main.py
+++ main.py
@@ -14,7 +14,25 @@ def introduction():
 ▓█████▓░░░░▒▓████▒▒████▓░▒█████▒▓████▒▒████▓▒████▓░░▓██▒██▓░░▓████▒░▒████▒░▓███▓
 """)
     print("Welcome to Pitchfork's Best of 2022!")
-    
+        
+def generate_album_graph(data_file):
+    albums = Graph()
+
+    for genre in genres.genres:
+        albums.add_vertex(Vertex(genre))
+        
+    with open(data_file, 'r') as f:
+        data = f.read()
+        
+    album_data = json.loads(data)
+
+    for entry in album_data:
+        album_vertex = Vertex(create_album(entry))
+        albums.add_vertex(album_vertex)
+        
+        for genre in entry['genres']:
+            albums.add_edge(albums.graph_dict[genre], album_vertex)
+
 def create_album(data):
     artist =        data['artist']
     album_title =   data['album_title']
