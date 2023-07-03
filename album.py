525b66589320fef8848e781fbed29d57aaa6f52b
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2023-07-03 19:23:20 +0530

    document code

--- album.py
+++ album.py
@@ -1,5 +1,15 @@
 class Album:
     def __init__(self, album_title, artist, genres, label, rating, review) -> None:
+        """Constructor for an album object
+
+        Args:
+            album_title (str): Name of album
+            artist (str): Name of the artist
+            genres (list): List of genres associated to the album
+            label (str): Name of label(s)
+            rating (float): Pitchfork rating
+            review (str): Pitchfork review
+        """        
         self.album_title = album_title
         self.artist = artist
         self.genres = genres
